6ce3d57dbe09d520919bbd58b53d2da3d00d6aa8
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-06-08 16:33:29 -0300

    Created class Game

--- src/pong/Game.java
+++ src/pong/Game.java
@@ -1,10 +1,43 @@
 package pong;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Dimension;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JFrame;
 
+public class Game extends Canvas implements Runnable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public static int WIDTH = 240;
+	public static int HEIGHT = 120;
+	public static int SCALE = 3;
+	
+	public Game() {
+		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
 	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		JFrame frame = new JFrame("Pong");
+		frame.setResizable(false); //não dimensiona a janela
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//fecha totalmente o jogo
+		
+		frame.add(game);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 
 }
